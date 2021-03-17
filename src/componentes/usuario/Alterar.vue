11ba19a4da3af29bbf631a800b99b9ddad47737e
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-17 10:43:28 -0300

    Resolving the mutation to change new user

--- src/componentes/usuario/Alterar.vue
+++ src/componentes/usuario/Alterar.vue
@@ -88,17 +88,55 @@ export default {
     },
     methods: {
         alterarUsuario() {
-            // Implementar
-        },
-        obterPerfis() {
-             this.$api.query({
-                query: gql `{perfis {id rotulo}}`
-            }).then(result =>{
-                console.log(result)
-                this.perfis = result.data.perfis
+            this.$api.mutate({
+                mutation: gql`mutation (
+                    $idFiltro: Int
+                    $emailFiltro: String
+                    $nome: String
+                    $email: String
+                    $senha: String
+                    $perfis: [PerfilFiltro]
+                ) {
+                    alterarUsuario (
+                        filtro: {
+                            id: $idFiltro
+                            email: $emailFiltro
+                        }
+                        dados: { 
+                            nome: $nome
+                            email: $email
+                            senha: $senha
+                            perfis: $perfis
+                        }
+                    ) { 
+                        id nome email perfis { rotulo }
+                    }
+                }`,
+                variables: {
+                    idFiltro: this.filtro.id,
+                    emailFiltro: this.filtro.email,
+                    nome: this.usuario.nome,
+                    email: this.usuario.email,
+                    senha: this.usuario.senha,
+                    perfis: this.perfisSelecionados
+                },
+            }).then(resultado => {
+                this.dados = resultado.data.alterarUsuario
+                this.filter = {}
+                this.usuario = {}
                 this.erros = null
             }).catch(e => {
-                 this.erros = e
+                this.erros = e
+            })
+        },
+        obterPerfis() {
+            this.$api.query({
+                query: gql`{ perfis { id rotulo } }`
+            }).then(resultado => {
+                this.perfis = resultado.data.perfis
+                this.erros = null
+            }).catch(e => {
+                this.erros = e
             })
         }
     }
